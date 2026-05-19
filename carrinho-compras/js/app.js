c75fde21321bc830eed6becd2e2e9087d30b494b
Leandro Ludwig Batista <ludwigleandro150@gmail.com>
2026-05-19 16:30:50 -0300

    Add files via upload

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,37 @@
+let precoFinal;
+limpar();
+
+function adicionar(){
+    let produto = document.getElementById('produto').value;
+    let nomeproduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade} x</span> ${nomeproduto}<span class="texto-azul">R$ ${preco}</span>
+        </section>`;
+    precoFinal = precoFinal + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${precoFinal}`;
+    document.getElementById('quantidade').value = 0;
+
+ 
+
+}
+function limpar(){
+precoFinal = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = 'R$ 0';
+
+
+}
+
+
+
+
+
+
+
+
+
